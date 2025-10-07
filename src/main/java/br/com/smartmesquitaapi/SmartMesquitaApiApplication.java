6c986ef02e266751f0497d1423057b487d84bc11
Álvaro Mashni <alvinhoremor.sm@gmail.com>
2025-10-07 16:04:27 -0300

    setup inicial
    feat: setup inicial Spring Boot configurado

---
+++ src/main/java/br/com/smartmesquitaapi/SmartMesquitaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.smartmesquitaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMesquitaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMesquitaApiApplication.class, args);
+    }
+
+}
