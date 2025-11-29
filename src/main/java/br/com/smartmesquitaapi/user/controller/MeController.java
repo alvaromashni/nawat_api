bb7d48e7a0a2c0d49f9daae55c09f4e3e0b848b4
Álvaro Mashni <alvinhoremor.sm@gmail.com>
2025-11-28 21:06:25 -0300

    feat: controller notification done

--- src/main/java/br/com/smartmesquitaapi/user/controller/MeController.java
+++ src/main/java/br/com/smartmesquitaapi/user/controller/MeController.java
@@ -1,14 +1,32 @@
 package br.com.smartmesquitaapi.user.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.smartmesquitaapi.user.dto.NotificationsSettingsDto;
+import br.com.smartmesquitaapi.user.service.NotificationSettingsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/api/v1/users/me")
 public class MeController {
 
-    @GetMapping("/notification-settings/get")
-    public
+    private final NotificationSettingsService notificationSettingsService;
+
+    public MeController(NotificationSettingsService notificationSettingsService) {
+        this.notificationSettingsService = notificationSettingsService;
+    }
+
+    @GetMapping("/notification-settings")
+    public ResponseEntity<NotificationsSettingsDto> getNotificationSettings(){
+        NotificationsSettingsDto notificationDto = notificationSettingsService.getNotificationSettings();
+        return ok().body(notificationDto);
+    }
+
+    @PutMapping("/notification-settings")
+    public ResponseEntity<Void> updateNotificationSettings(@RequestBody  NotificationsSettingsDto notificationDto){
+        notificationSettingsService.updateNotificationSettings(notificationDto);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/br/com/smartmesquitaapi/user/service/NotificationSettingsService.java
+++ src/main/java/br/com/smartmesquitaapi/user/service/NotificationSettingsService.java
@@ -1,8 +1,14 @@
 package br.com.smartmesquitaapi.user.service;
 
 import br.com.smartmesquitaapi.user.UserRepository;
+import br.com.smartmesquitaapi.user.domain.Notification;
+import br.com.smartmesquitaapi.user.domain.User;
 import br.com.smartmesquitaapi.user.dto.NotificationsSettingsDto;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class NotificationSettingsService {
@@ -14,6 +20,33 @@ public class NotificationSettingsService {
     }
 
     public NotificationsSettingsDto getNotificationSettings(){
-        userRepository.
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("Email não encontrado."));
+
+        Notification notification = user.getNotification();
+        NotificationsSettingsDto notificationDto = new NotificationsSettingsDto();
+
+        notificationDto.setDailySummary(notification.isDailySummary());
+        notificationDto.setDonationDone(notification.isDonationDone());
+        notificationDto.setTotemMaintenance(notification.isTotemMaintenance());
+
+        return notificationDto;
+    }
+
+    public void updateNotificationSettings(NotificationsSettingsDto notificationDto){
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("Email não encontrado."));
+
+        Notification notification = user.getNotification();
+
+        notification.setDailySummary(notificationDto.isDailySummary());
+        notification.setDonationDone(notificationDto.isDonationDone());
+        notification.setTotemMaintenance(notificationDto.isTotemMaintenance());
+
+        userRepository.save(user);
     }
 }
